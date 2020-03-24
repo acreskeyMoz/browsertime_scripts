254513c2f96db324034e081105ea31aa6306a717
Andrew Creskey <44072237+acreskeyMoz@users.noreply.github.com>
2020-03-24 17:12:33 -0400

    Added Chrome configuration shell

---
+++ chrome.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+bash ./clear_apps.sh
+
+if [[ -n $ANDROID_SERIAL ]] ; then
+    DEVICE_SERIAL_ARGS="--firefox.android.deviceSerial=$ANDROID_SERIAL --chrome.android.deviceSerial=$ANDROID_SERIAL"
+else
+    DEVICE_SERIAL_ARGS=
+fi
+
+# N.B.: yargs doesn't parse `--firefox.android.intentArgument --ez`
+# properly, so always use `=--ez`!
+$BROWSERTIME_BIN \
+    --android \
+    --skipHar \
+    $DEVICE_SERIAL_ARGS \
+    "$@"
+
+adb -s $ANDROID_SERIAL shell am force-stop $PACKAGE
