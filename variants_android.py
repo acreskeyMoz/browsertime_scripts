ab48ee4b702da297fd113a491b049b4c8ece5b3e
Andrew Creskey <44072237+acreskeyMoz@users.noreply.github.com>
2020-12-18 15:56:16 -0500

    Update variants_android.py
    typo

--- variants_android.py
+++ variants_android.py
@@ -6,7 +6,7 @@ fenix_beta = '~/dev/experiments/fennec_gve_fenix/binaries/fenix.beta.2020.11.30.
 fennec68 = '~/dev/experiments/fennec_gve_fenix/binaries/fennec-68.3.0.multi.android-aarch64.apk'
 fenix_nightly = '~/dev/experiments/fennec_gve_fenix/binaries/fenix.nightly.2020.12.16_aarch64.apk'
 
-# the lass parameter can be used to pass through a firefox preference. eg. ('--firefox.preference network.preload:false ')
+# the last parameter can be used to pass through a firefox preference. eg. ('--firefox.preference network.preload:false ')
 variants = [ ('fenix', 'fenix.sh', 'org.mozilla.fenix', fenix_nightly, '' ),
              ('fenix_beta', 'fenix.sh', 'org.mozilla.firefox_beta', fenix_beta, ' ' ),
              ('fennec68', 'fennec.sh', 'org.mozilla.firefox', fennec68, '')]
